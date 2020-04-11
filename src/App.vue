6fbc9a47b8184c1f65a33ec3746c9e4baf7b5c61
oussama abai <oussamaabai@gmail.com>
2020-04-11 14:29:47 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <img width="25%" src="./assets/logo.png">
+    <Blogspot/>
+
+  </div>
+</template>
+
+<script>
+import Blogspot from "./components/blog";
+
+export default {
+  name: "App",
+  components: {
+    Blogspot
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/blog.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <h1>Articles</h1>
+    <div v-for="post in blogdata" :key="post.id">{{post.title}}</div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "HelloWorld",
+  data() {
+    return {
+      blogdata: []
+    };
+  },
+  created() {
+    axios
+      .get(
+        // API KEy : https://developers.google.com/blogger/docs/3.0/using#WorkingWithBlogs
+        // BlogID : open your blogspot account
+        "https://www.googleapis.com/blogger/v3/blogs/8060917798751958753/posts?key=AIzaSyBEhx__zclxQ4YqzZCaxHfNaf5pmNjxfIk"
+      )
+      .then(Response => {
+        console.log(Response);
+        this.blogdata = Response.data.items;
+      })
+      .catch(Error => {
+        console.log(Error);
+      });
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
